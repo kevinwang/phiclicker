f8ff03caf05741cd9dd38eb2c548713e56d8074a
Kevin Wang <kevin@kevinwang.com>
2015-10-18 03:22:29 -0500

    Fix schema of Responses.

--- models/response.js
+++ models/response.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var Response = sequelize.define('Response', {
-        UserId: {
-            type: DataTypes.INTEGER,
+        UserUsername: {
+            type: DataTypes.STRING,
             primaryKey: true
         },
         QuestionId: {
